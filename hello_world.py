8bfedb0152b44ea1762af44396bb71c4d80eacf9
Charles Ivia <iviacharles@gmail.com>
2020-09-20 13:26:05 +0300

    Hello world

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+
+#Area = Pi*radius**2
+
+pi = 3.14
+
+
+def area_of_circle(pi):
+    rad = float(input("What is your radius? "))
+    print(pi * rad ** 2)
+
+
+area_of_circle(pi)
